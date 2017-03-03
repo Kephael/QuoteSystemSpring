533527d62da5376f3dbc4fc565d8546595911411
Kephael <me@caseybaer.com>
2017-03-02 21:29:45 -0500

    Added search for quotes by username functionality and fixed corresponding QuoteRepository method signature

--- src/main/java/com/quotesystem/form/QuoteRepository.java
+++ src/main/java/com/quotesystem/form/QuoteRepository.java
@@ -1,11 +1,13 @@
 package com.quotesystem.form;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface QuoteRepository extends MongoRepository<Quote, String> { // MongoDB quote data collection which will be autowired
 
 	public Quote findById(String id);
 
-	public Quote findByUsername(String username);
+	public List<Quote> findByUsername(String username);
 
 }
