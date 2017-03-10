cea849078cf92a4ce66e674ea3427eb36bc312da
Kephael <me@caseybaer.com>
2017-03-09 22:55:18 -0500

    Added Spring Security Test dependency to the project and now loggeduser integration test is working for login controller

--- src/test/java/datacontrollers/QuoteRestControllerTest.java
+++ src/test/java/datacontrollers/QuoteRestControllerTest.java
@@ -1,14 +1,24 @@
 package datacontrollers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.quotesystem.QuoteSystemSpring3Application;
 
@@ -18,10 +28,27 @@ public class QuoteRestControllerTest {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
+	@Autowired
+	private WebApplicationContext context;
+	private MockMvc mvc;
+
+	@Before
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+	}
 
 	@Test
-	public void loginTest() {
+	public void invalidLoginTest() {
 		String body = restTemplate.getForObject("/authenticate", String.class);
-		assertThat(body).contains("Unauthorized");
+		assertTrue(body.contains("Unauthorized"));
+	}
+
+	@Test
+	@WithMockUser(username = "admin", roles = { "USER", "ADMIN" })
+	public void validLoginTest() throws Exception {
+		MvcResult result = mvc.perform(get("/authenticate")).andReturn();
+		assertTrue(result.getResponse().getContentAsString().contains("true"));
+
+
 	}
 }
