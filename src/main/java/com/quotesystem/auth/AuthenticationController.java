7a11c1e02310233034fd533044a9480ef5f68103
Kephael <me@caseybaer.com>
2017-04-01 10:22:36 -0400

    Return AuthData instance on successful login at /authenticate end point

--- src/main/java/com/quotesystem/auth/AuthenticationController.java
+++ src/main/java/com/quotesystem/auth/AuthenticationController.java
@@ -1,5 +1,10 @@
 package com.quotesystem.auth;
 
+import java.util.ArrayList;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,13 @@ public class AuthenticationController {
 	@RequestMapping(value = "/authenticate")
 	public AuthData login() {
 		AuthData data = new AuthData();
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    data.setUsername(auth.getName());
+	    ArrayList<String> authoritiesList = new ArrayList<String>();
+	    for (GrantedAuthority authority : auth.getAuthorities()){
+	    	authoritiesList.add(authority.getAuthority());
+	    }
+	    data.setRoles(authoritiesList);
 		return data;
 	}
 
